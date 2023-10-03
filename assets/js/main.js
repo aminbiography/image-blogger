64e007fd582f6c04d2831105a1ef867c7c117620
Mohammad Aminul Islam <biographyamin@gmail.com>
2023-10-04 01:45:37 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Author: Mohammad Aminul Islam  //
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+
+  function openWin() {
+    window.open("https://www.paypal.com/signin");
+  }
+
+
+
+
